4ec5554f1c2427a17d6851d43358be8fa006c4b6
Oliver <j.oliver.vv@gmail.com>
2026-02-14 15:48:53 -0600

    push

---
+++ module_4/docs/_build/html/_sources/api.rst.txt
@@ -0,0 +1,14 @@
+API Reference
+=============
+
+.. automodule:: src.flask_app
+   :members:
+
+.. automodule:: src.etl
+   :members:
+
+.. automodule:: src.query_data
+   :members:
+
+.. automodule:: src.db
+   :members:
---
+++ module_4/docs/source/conf.py
@@ -0,0 +1,5 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
+project = "Grad Café Analytics (Module 4)"
+extensions = ["sphinx.ext.autodoc","sphinx.ext.napoleon","sphinx.ext.viewcode"]
+html_theme = "sphinx_rtd_theme"
